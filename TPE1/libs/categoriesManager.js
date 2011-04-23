ea740d627af1962c85ba676c509f68dff1cad369
Juan Pablo Orsay <jporsay@gmail.com>
2011-04-23 02:46:35 -0300

    changes changes...

--- TPE1/libs/categoriesManager.js
+++ TPE1/libs/categoriesManager.js
@@ -1,15 +1,28 @@
 
-function loadCategories(element) {
-	//element es el id o clase del div en el que se van a meter todas las catgorias
-	var categories = [{id: sports, img: images/sports.png},{id: health, img: images/health.png},{id: technology, img: images/technology.png}];
-	var node = ($document).getElementsById(element);
+function loadCategories(element, categories) {
+	//element is the ID of the category container.
+	//categories is an array of maps holding information of each category.
+	//function should be something like this... too sleepy to debug...
+	var elDOM = document.getElementById(element);
 	
-	for (category in categories) {
+	for (var i = 0; i < categories.length; ++i) {
+		var category = categories[i];
+		var new_category = document.createElement("div");
+		new_category.setAttribute("class", "category")
 		
+		var new_cat_image = document.createElement("img");
+		new_cat_image.setAttribute("src", category.img);
+		new_cat_image.setAttribute("class", "image");
+		
+		var new_cat_p = document.createElement("p");
+		new_cat_p.setAttribute("id", category.id);
+		
+		new_category.appendChild(new_cat_image);
+		new_category.appendChild(new_cat_p);
+		elDOM.appendChild(new_category);
 	}
 }
 
-function createCategory(name, imagesrc) {
-	this.name = name;
-	this.imagesrc = imagesrc;
+function createCategory(c_name, c_imagesrc) {
+	return {name: c_name, img: c_imagesrc};
 }
