3dfa9be6825a06fb91448b90dc18da98128c322a
Juan Pablo Orsay <jporsay@gmail.com>
2011-05-11 19:00:25 -0300

    added post...

--- TPE1/libs/ServerManager.js
+++ TPE1/libs/ServerManager.js
@@ -2,6 +2,7 @@ function ServerManager(section) {
 	this.section = section;
 }
 
+//Synchronic ajax
 ServerManager.prototype.getS = function(params, success, error) {
 	$.ajax({
 		type: 'GET',
@@ -14,12 +15,24 @@ ServerManager.prototype.getS = function(params, success, error) {
 	});
 }
 
+//Asynchronic ajax
 ServerManager.prototype.get = function(params, success, error) {
 	$.ajax({
 		type: 'GET',
 		url: '/service/' + this.section + '.groovy',
 		data: params,
 		dataType: 'xml',
+		success: success,
+		error: error
+	});
+}
+
+ServerManager.prototype.post = function(params, success, error) {
+	$.ajax({
+		type: 'POST',
+		url: '/service/' + this.section + '.groovy',
+		data: params,
+		datatType: 'xml',
 		success: success,
 		error: error
 	});
