57c0797c0b670956251c1399bcda03820e3afc50
Juan Pablo Orsay <jporsay@gmail.com>
2011-05-11 05:07:44 -0300

    various fixes.

--- TPE1/libs/subcategoriesManager.js
+++ TPE1/libs/subcategoriesManager.js
@@ -1,13 +1,10 @@
-<div class='subcategory'>
-					<span class='subcategorytitle'>Action Sports</span><br/>
-				</div>
 var catalog = new ServerManager('Catalog');
 
 function getSubCategories(categoryId) {
 	catalog.get(
 		{
 			method: 'GetSubcategoryList',
-			language_id: langId,
+			language_id: 1,
 			category_id: categoryId,
 		},
 		processSubcategories
@@ -17,11 +14,23 @@ function getSubCategories(categoryId) {
 function processSubcategories(data) {
 	$(data).find('subcategory').each(
 		function() {
-			createSubCategory($(this).attr('id'), $(this).find('name').text());
+			createSubCategory(
+				$(this).attr('id'),
+				$(this).find('category_id').text(),
+				$(this).find('name').text(),
+				document.getElementsByClassName('subcategoriescontainer')[0]
+			);
 		}
 	)
 }
 
-function createSubCategory(catId, catName) {
+function createSubCategory(subCatId, catId, subCatName, container) {
+	var temp = null;
+	
+	temp = document.createElement('div');
+	temp.setAttribute('class', 'subcategory');
+	temp.innerHTML = '<a href=\'browse.html?subCatId=' + subCatId + '&catId=' + catId + '\' class=\'subcategorytitle\'>' + subCatName + '</a><br/>';
+	
+	container.appendChild(temp);
 	
 }
