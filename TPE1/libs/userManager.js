62c7a28efd5128094a4cb281f832e157cf00d544
Juan Pablo Orsay <jporsay@gmail.com>
2011-05-11 19:01:04 -0300

    added temporary registration

--- TPE1/libs/userManager.js
+++ TPE1/libs/userManager.js
@@ -1,6 +1,36 @@
 var security = new ServerManager('Security');
 
 function createUser(rawData) {
+	var processedData = processData(rawData);
+	var nuXML = buildNewUserXML(processedData);
+	alert('asd');
+	security.post({
+		method: 'CreateAccount',
+		account: nuXML
+	},
+		successPost,
+		errorPost
+	);
+}
+
+function successPost(data) {
+	alert($(data).find('response').attr('status'));
+}
+
+function errorPost(data) {
+	alert($(data).find('response').attr('status'));
+}
+
+function processData(rawData) {
+	var pData = [];
+	rawData.each(
+		function() {
+			if ($(this).attr('type') !== 'submit' && $(this).attr('type') !== 'button') {
+				pData[$(this).attr('id')] = $(this).val();
+			}
+		}
+	)
+	return pData;
 }
 
 function buildNewUserXML(rawData) {
