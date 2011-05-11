445d89d5a358279fad80811e11ac3404cd742e7a
Juan Pablo Orsay <jporsay@gmail.com>
2011-05-11 04:02:47 -0300

    many changes. loading categories from service works.

--- TPE1/libs/categoriesManager.js
+++ TPE1/libs/categoriesManager.js
@@ -1,6 +1,6 @@
 var catalog = new ServerManager('Catalog');
 
-function loadCategories(element, categories) {
+function loadCategories() {
 	catalog.get(
 		{
 			method: 'GetCategoryList',
@@ -13,7 +13,8 @@ function loadCategories(element, categories) {
 function processCategories(data) {
 	$(data).find('category').each(
 		function() {
-			showCategory($(this).find('id').text(), $(this).find('name').text());
+			var category = $(this);
+			showCategory(category.attr('id'), category.find('name').text());
 		}
 	);
 }
@@ -21,29 +22,26 @@ function processCategories(data) {
 function showCategory(id, name) {
 	//element is the ID of the category container.
 	//categories is an array of maps holding information of each category.
-	var elDOM = document.getElementById(element);	
+	var elDOM = document.getElementById('categorycontainer');	
 	var new_category = document.createElement("div");
 	new_category.setAttribute('id', id);
 	new_category.setAttribute("class", "category");
 	
 	var new_cat_image = document.createElement("img");
-	new_cat_image.setAttribute("src", category.img);
+	new_cat_image.setAttribute("src", '');
 	new_cat_image.setAttribute("alt", name);
 	
 	var new_cat_desc = document.createElement("div");
 	new_cat_desc.setAttribute("class", "catdescription");
 	
 	var link = document.createElement("a");
-	link.setAttribute("id", category.id);
-	link.setAttribute("class", "i18n");
-	link.setAttribute("href","#");
+	link.setAttribute("id", id);
+	//link.setAttribute("class", "i18n");
+	link.innerHTML = name;
+	link.setAttribute("href","subcategories.html?catId=" + id);
 	new_cat_desc.appendChild(link);
 	
 	new_category.appendChild(new_cat_image);
 	new_category.appendChild(new_cat_desc);
 	elDOM.appendChild(new_category);
-}
-
-function createCategory(c_name, c_imagesrc) {
-	return {name: c_name, img: c_imagesrc};
 }
