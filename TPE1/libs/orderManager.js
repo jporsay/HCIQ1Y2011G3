5367df848228de07de4c3495cb9a554738220f3e
unknown <Gonzalo@.(none)>
2011-05-25 09:30:52 +0430

    Actualizado un poco mas el orderTracking. Falta ver porque no se imprimen los items.

--- TPE1/libs/orderManager.js
+++ TPE1/libs/orderManager.js
@@ -7,6 +7,7 @@ var _DELIVERED = "4";
 var _userdata;
 
 var orderServer = new ServerManager('Order');
+var createdOrdersEl, confirmedOrdersEl, deliveringOrdersEl, deliveredOrdersEl;
 
 function printOrders(userdata, langId) {
 	_userdata = userdata;
@@ -21,30 +22,14 @@ function printOrders(userdata, langId) {
 }
 		
 function processOrders(orders) {
-	var createdOrdersEl = document.getElementById('createdOrders');
-	var confirmedOrdersEl = document.getElementById('confirmedOrders');
-	var deliveringOrdersEl = document.getElementById('deliveringOrders');
-	var deliveredOrdersEl = document.getElementById('deliveredOrders');
-	
+	createdOrdersEl = document.getElementById('createdOrders');
+	confirmedOrdersEl = document.getElementById('confirmedOrders');
+	deliveringOrdersEl = document.getElementById('deliveringOrders');
+	deliveredOrdersEl = document.getElementById('deliveredOrders');
 	$(orders).find('order').each(
 		function() {
 			var order = $(this);
-			var element;
-			var status = order.find('status').text();
-			switch(status) {
-				case _CREATED :
-					createOrderDiv(createdOrdersEl, order, 'created');
-					break;
-				case _CONFIRMED: 
-					createOrderDiv(confirmedOrdersEl, order, 'confirmed');
-					break;
-				case _DELIVERING:
-					createOrderDiv(deliveringOrdersEl, order, 'shipped');
-					break;
-				case _DELIVERED:
-					createOrderDiv(deliveredOrdersEl, order, 'delivered');
-					break;
-			}
+			createOrderDiv(order);
 		}
 	);
 }
@@ -56,41 +41,53 @@ function processOrders(orders) {
 	<span class='i18n'>Date:</span>
 	<span class='date'>1999-10-10</span>
 	<div class="itemsTable"></div>
-	<button class="delete">Delete Order</button>
-	<button class="confirm">Confirm Order</button>
 </div>
 */
-function createOrderDiv(parent, order, orderType) {
+function createOrderDiv(order, orderType) {
+	var orderType, parent;
+	var status = order.find('status').text();
+	switch(status) {
+		case _CREATED :
+			orderType = 'created';
+			parent = createdOrdersEl;
+			break;
+		case _CONFIRMED: 
+			orderType = 'confirmed';
+			parent = confirmedOrdersEl;
+			break;
+		case _DELIVERING:
+			orderType = 'shipped';
+			parent = deliveringOrdersEl;
+			break;
+		case _DELIVERED:
+			orderType = 'delivered';
+			parent = deliveredOrdersEl;
+			break;
+	}
 	var newOrder = document.createElement('div');
 		newOrder.setAttribute('class', 'order');
 		newOrder.setAttribute('id', 'order' + order.attr('id'));
-	
-	var clear = document.createElement('div');
-		clear.setAttribute('class', 'clear');
-		
-	var order_address_label = document.createElement('span');
-		order_address_label.setAttribute('class', 'i18n label');
-		order_address_label.innerHTML = 'Address: ';
-	var order_address = document.createElement('span');
-		order_address.setAttribute('class', 'address');
-	var addressId = order.find('address_id').text();
-		order_address.innerHTML = addressId ? addressId : 'Undefined Address';		
-	newOrder.appendChild(order_address_label);
-	newOrder.appendChild(order_address);
-	newOrder.appendChild(clear);
-	
-	var order_date_label = document.createElement('span');
-		order_date_label.setAttribute('class', 'i18n label');
-		order_date_label.innerHTML = 'Date: ';
-	var order_date = document.createElement('span');
-		order_date.setAttribute('class', 'date');
-		order_date.innerHTML = order.find(orderType + '_date').text();
-	newOrder.appendChild(order_date_label);
-	newOrder.appendChild(order_date);
+	createLabel(newOrder, 'Order ID: ', order.attr('id'), 'orderId');	
+	createLabel(newOrder, 'Address: ', order.find('address_id').text(), 'address');
+	createLabel(newOrder, 'Date: ', order.find(orderType + '_date').text(), 'date');
 
 	var table_container = createOrderPruductTable(order, '$');
 	newOrder.appendChild(table_container);
 	parent.appendChild(newOrder);
+}
+
+function createLabel(parent, label, value, valueClass) {
+	var clear = document.createElement('div');
+	clear.setAttribute('class', 'clear');
+	var label_span = document.createElement('span');
+		label_span.setAttribute('class', 'i18n label');
+		label_span.innerHTML = label;
+	var label_value = document.createElement('span');
+		label_value.setAttribute('class', valueClass);
+		label_value.innerHTML = value;		
+	parent.appendChild(label_span);
+	parent.appendChild(label_value);
+	parent.appendChild(clear);
 }
 
 function createOrderPruductTable(order, currency) {
@@ -98,13 +95,15 @@ function createOrderPruductTable(order, currency) {
 	var order_table_container = document.createElement('div');
 		order_table_container.setAttribute('class', 'itemsTable');
 
-	var items = getProductsFor(orderId);
-	createItemsTableInElement(order_table_container, items, currency);
+	getProductsFor(orderId);
+	//Se supone que retorna una lista de productos correspondientes a la orden ordenID
+	//Como no logro conseguir eso, se estan guardando en productsInOrder(que no guarda nada no se porque).
+	createItemsTableInElement(order_table_container, productsInOrder, currency);
 	return order_table_container;
 }
 
+var productsInOrder = [];
 function getProductsFor(orderId) {
-	var productsInOrder = [];
 	orderServer.get(
 		{
 			method: 'GetOrder',
@@ -113,35 +112,45 @@ function getProductsFor(orderId) {
 			order_id: orderId
 		},
 		function(data) {
+			console.log(data);
 			$(data).find('item').each (
 				function() {
 					var item = $(this);
-					var product = getProduct(item.find('product_id').text());
+					getProduct(item.find('product_id').text());
+					//Se supone q devuelve un proucto, pero como se dicidio a no devolver nada,
+					//el producto se etsa guardando en prod y lo copio a esta nuava variable producto para guardarlo
+					var product = {
+						name: prod.name, 
+						id: prod.id,
+						price: prod.price,
+					}
 					product.quantity = item.find('count').text();
 					productsInOrder.push(product);
+					console.log('nuevo item ' + product.name);
 				}
 			);
 		}
 	);
-	return productsInOrder;
 }
 
 function getProduct(productId) {
-	orderServer.get(
+	var catalogServer = new ServerManager('Catalog');
+	catalogServer.get(
 		{
 			method: 'GetProduct',
 			product_id: productId
 		},
-		function(data) {
-			var item = $(data).find('product');
-			var prod = {
-				name: item.find('name').text(), 
-				id: item.attr('id'), 
-				price: item.find('price').text(),
-				imageSrc: ''
-			}
-			return prod;
-		}
+		createItem
 	);
+}
+
+var prod;
+function createItem(data) {
+	var item = $(data).find('product');
+	prod = {
+		name: item.find('name').text(), 
+		id: item.attr('id'), 
+		price: item.find('price').text(),
+	}
 }
 
